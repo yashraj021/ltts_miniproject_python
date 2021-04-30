19b4f3d6f1a67f36f7dcda4fe3dee1b9d14e4660
Yash <yash.raj021@gmail.com>
2021-04-30 23:00:26 +0530

    PyTest added. Project Complete

--- index.py
+++ index.py
@@ -1,15 +1,46 @@
+import pytest
 
-# Get the use e-mail address
-email = input("Enter your E-mail address: ").strip()
+@pytest.fixture
+def credentials():
+    email = "yash.raj021@gmail.com"
+    username = "yash.raj021"
+    domain = "gmail.com"
 
-#Slicing out username
-user_name = email[:email.index("@")]
+    return (email, username, domain)
+
+def test_method_1(credentials):
+    email = get_email(credentials[0])
+    
+    user_name = get_user_name(email)
+    
+    domain_name = get_domain_name(email)
+
+    assert credentials[0] == email
+    assert credentials[1] == user_name
+    assert credentials[2] == domain_name
+
+
+def get_email(email):
+
+    return email
+
+def get_user_name(email):
+    
+    user_name = email[:email.index("@")]
+    
+    return user_name
 
 #Slicing out domain name
-domain_name = email[email.index("@")+1:]
 
-#Format message
-result = f"Your username is:- '{user_name}' and domain name is:- '{domain_name}'"
+def get_domain_name(email):
+    
+    domain_name = email[email.index("@")+1:]
+    
+    return domain_name
 
-#Priniting result
-print(result)
+def formatter(user_name, domain_name):
+
+    result = f"Your username is:- '{user_name}' and domain name is:- '{domain_name}'"
+
+    return result
+
